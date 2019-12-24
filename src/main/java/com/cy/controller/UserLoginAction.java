d4c866e7470471b377453ccff2e35008bd5a2cdb
caoyi <3108815218@qq.com>
2019-12-24 16:32:12 +0800

    登陆

--- src/main/java/com/cy/controller/UserLoginAction.java
+++ src/main/java/com/cy/controller/UserLoginAction.java
@@ -1,7 +1,6 @@
 package com.cy.controller;
 
 import com.cy.service.UserLoginService;
-import com.cy.service.impl.UserLoginServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +24,10 @@ public class UserLoginAction {
     private static final Logger logger = LoggerFactory.getLogger(UserLoginAction.class);
     @Autowired
     private UserLoginService userLoginService ;
-    
-    
-    
-    
+
+
+
+
     /**
      * @Author cy
      * @Date 2019/12/23 16:26
